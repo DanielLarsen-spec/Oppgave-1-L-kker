25df85a08664617bdf7242d3f5e0cc84ad33d48f
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-09-18 14:32:27 +0200

    Initial commit

---
+++ controller.js
@@ -0,0 +1,14 @@
+
+function numCounter(){
+
+let stopValue = 20;
+let numToPush = 0;
+num = []
+for(let counter = 0; counter <= stopValue; counter++ ){
+    num.push(numToPush)
+    numToPush += 2
+}
+
+updateView()
+
+}
---
+++ view.js
@@ -0,0 +1,13 @@
+updateView()
+function updateView(){
+document.getElementById("app").innerHTML = /*HTML*/`
+
+
+<div>${num}</div>
+
+<button onclick="numCounter()">counter</button>
+
+`;
+
+
+}
